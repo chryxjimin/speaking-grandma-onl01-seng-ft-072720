edb4de3e5325df1abb2d2aa252bb345e12788887
Christie Yoon <cleex1999@gmail.com>
2020-07-29 03:07:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,13 @@
+def speak_to_grandma(phrase)
+   if phrase == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU TOO PUMPKIN"
+   elsif phrase == phrase.downcase
+      puts "HUH?! SPEAK UP, SONNY!"
+   else
+      puts "NO, NOT SINCE 1938!"
+   end
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
